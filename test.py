2b95174cdce1321a9c7876d2279801d0601e0267
Prashanth1998-18 <pa500@scarletmail.rutgers.edu>
2024-03-22 20:37:07 -0400

    Streamlit app developed

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 from src.mcqgenerator.logger import logging
 
-logging.info("Starting mcq generator")
+#logging.info("Starting mcq generator")
+
+from dotenv import load_dotenv,dotenv_values
+
+load_dotenv()
+config = dotenv_values(".env")
+print(config["OPENAI_API_KEY"])
